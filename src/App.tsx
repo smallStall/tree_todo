283beef1f092ca6add7165e8b4993f8ace5fbc45
smallstall001 <cakbj001@gmail.com>
2023-03-12 18:50:54 +0900

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,184 +1,98 @@
-import { forwardRef, useState } from "react";
-import TreeView from "@mui/lab/TreeView";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import DoneIcon from "@mui/icons-material/Done";
-import { TextField, IconButton, Box } from "@mui/material";
-import TreeItem, {
-  TreeItemProps,
-  useTreeItem,
-  TreeItemContentProps,
-} from "@mui/lab/TreeItem";
-import clsx from "clsx";
+import { memo, useState } from "react";
+import { atom, useAtomValue, useSetAtom } from "jotai";
+import type { Atom, WritableAtom } from "jotai";
 
-const CustomContent = forwardRef(function CustomContent(
-  props: TreeItemContentProps,
-  ref
-) {
-  const {
-    classes,
-    className,
-    label,
-    nodeId,
-    icon: iconProp,
-    expansionIcon,
-    displayIcon,
-  } = props;
+type Node = {
+  text: string;
+  value: AtomNode;
+  childNodes: Atom<AtomNode[]>;
+};
 
-  const {
-    disabled,
-    expanded,
-    selected,
-    focused,
-    handleExpansion,
-    handleSelection,
-    preventSelection,
-  } = useTreeItem(nodeId);
-  const [disable, setDisable] = useState(disabled);
+type AtomNode = WritableAtom<Node, Partial<Node>[], void>;
 
-  const icon = iconProp || expansionIcon || displayIcon;
+const rootNodes = atom([]);
+const appendNode = atom(null, (_get, set, [nodes, value]) => {
+  const node = atom({
+    value: atom(value),
+    childNodes: atom([]),
+  });
+  set(nodes, (prev: AtomNode[]) => [...prev, node]);
+});
 
-  const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    preventSelection(event);
+const total = atom((get) => {
+  const sum = (nodes: Atom<AtomNode[]>): number => {
+    const arr = get(nodes);
+    return arr.reduce((p: number, c: AtomNode) => {
+      return p + 1 + sum(get(c).childNodes);
+    }, 0);
   };
+  return { count: sum(rootNodes) };
+});
 
-  const handleExpansionClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    handleExpansion(event);
-  };
+const TotalCount = () => {
+  const { count } = useAtomValue(total);
+  return <div>Total: {count}</div>;
+};
 
-  const handleSelectionClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    handleSelection(event);
-  };
-  if (!label) return null;
+const NodeValue = ({ value }: { value: AtomNode }) => {
+  const val = useAtomValue(value);
+  const set = useSetAtom(value);
+
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div
-      className={clsx(className, classes.root, {
-        [classes.expanded]: expanded,
-        [classes.selected]: selected,
-        [classes.focused]: focused,
-        [classes.disabled]: disable,
-      })}
-      onMouseDown={handleMouseDown}
-      ref={ref as React.Ref<HTMLDivElement>}
-    >
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-      <div onClick={handleExpansionClick} className={classes.iconContainer}>
-        {icon}
-        {!expansionIcon && (
-          <IconButton onClick={() => setDisable((prev) => !prev)}>
-            <DoneIcon />
-          </IconButton>
-        )}
-      </div>
-      <TextField
-        onClick={handleSelectionClick}
-        onKeyDown={(e) => {
-          if (e.code === "Enter") {
-            console.log("tacseta");
-          }
-        }}
-        className={classes.label}
-        disabled={disable}
-        defaultValue={label.content}
-        variant="standard"
-        sx={{ textDecoration: disable ? "line-through" : "none" }}
-      ></TextField>
-      <TextField
-        onClick={handleSelectionClick}
-        disabled={disable}
-        defaultValue={label.value}
-        variant="standard"
-        type="tel"
-        sx={{
-          width: "4rem",
-          textDecoration: disable ? "line-through" : "none",
-          "&::after": {
-            position: "absolute",
-            content: '"%"',
-            display: "inline",
-            left: "1.8rem",
-            top: "0.2rem",
-          },
-        }}
-      ></TextField>
+    <div>
+      <input value={val.text} onChange={(e) => set({ text: e.target.value })} />
     </div>
+  );
+};
+
+const Node = memo(({ node }: { node: AtomNode }) => {
+  const { value, childNodes } = useAtomValue(node);
+  return (
+    <li>
+      <NodeValue value={value} />
+      <Nodes nodes={childNodes} />
+    </li>
   );
 });
 
-function CustomTreeItem(props: TreeItemProps) {
+const Nodes = ({ nodes }: { nodes: Atom<AtomNode[]> }) => {
   return (
-    <TreeItem ContentComponent={CustomContent} {...props} disabled={false} />
+    <ul>
+      {useAtomValue(nodes).map((node: AtomNode) => (
+        <Node key={`${node}`} node={node} />
+      ))}
+      <NewNode nodes={nodes} />
+    </ul>
   );
-}
-
-type NodeState = {
-  content: string;
-  value: number;
-  disable: boolean;
 };
 
-type Node = {
-  id: string;
-  state: NodeState;
-  children?: Node[];
+const NewNode = ({ nodes }: { nodes: Atom<AtomNode[]> }) => {
+  const append = useSetAtom(appendNode);
+  const [text, setText] = useState("");
+  const add = () => {
+    append([nodes, { text }]);
+    setText("");
+  };
+  return (
+    <li>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter new text..."
+      />
+      <button disabled={!text} onClick={add}>
+        Add
+      </button>
+    </li>
+  );
 };
 
-function renderNode(node: Node) {
-  return (
-    <CustomTreeItem
-      nodeId={node.id}
-      label={{ content: node.state.content, value: node.state.value }}
-    >
-      {node.children && node.children.map((child) => renderNode(child))}
-    </CustomTreeItem>
-  );
-}
+const App = () => (
+  <>
+    <TotalCount />
 
-export default function IconExpansionTreeView() {
-  //zustandを使う実装にする
-  const [node, setNode] = useState({
-    id: "1",
-    state: {
-      content: "Root",
-      value: 10,
-      disable: false,
-    },
-    children: [
-      {
-        id: "2",
-        state: {
-          content: "Child 1",
-          value: 20,
-          disable: false,
-        },
-        children: [
-          {
-            id: "3",
-            state: {
-              content: "Child 1.1",
-              value: 30,
-              disable: false,
-            },
-          },
-        ],
-      },
-    ],
-  });
-  return (
-    <TreeView
-      aria-label="icon expansion"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 400, flexGrow: 1, width: 400, overflowY: "auto" }}
-    >
-      {renderNode(node)}
-    </TreeView>
-  );
-}
+    <Nodes nodes={rootNodes} />
+  </>
+);
+
+export default App;
